0bfc9b28da129dfe2fe7c56c0e53995e9c8b33c8
Toni Baker <tonijbaker5@gmail.com>
2022-10-29 18:33:21 -0400

    viewing ordered categories

--- TabloidMVC/Repositories/CategoryRepository.cs
+++ TabloidMVC/Repositories/CategoryRepository.cs
@@ -15,7 +15,7 @@ namespace TabloidMVC.Repositories
                 conn.Open();
                 using (var cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = "SELECT id, name FROM Category";
+                    cmd.CommandText = "SELECT id, Name FROM Category ORDER By Name ";
                     var reader = cmd.ExecuteReader();
 
                     var categories = new List<Category>();
@@ -25,7 +25,7 @@ namespace TabloidMVC.Repositories
                         categories.Add(new Category()
                         {
                             Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("name")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
                         });
                     }
 
