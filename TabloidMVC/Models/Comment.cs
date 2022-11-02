16db46fa19f4b4c2953ae6a9feaa8db1075924f9
Allison Carte <cartemack@gmail.com>
2022-11-02 13:16:20 -0400

    Created Comment model

---
+++ TabloidMVC/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TabloidMVC.Models
+{
+    public class Comment
+    {
+        // select Id, PostId, UserProfileId, Subject, Content, CreateDateTime from comment
+
+            public int Id { get; set; }
+            public int PostId { get; set; }
+            public int UserProfileId { get; set; }
+            public string Subject { get; set; }
+            public string Content { get; set; }
+            public DateTime CreateDateTime { get; set; }
+    }
+}
