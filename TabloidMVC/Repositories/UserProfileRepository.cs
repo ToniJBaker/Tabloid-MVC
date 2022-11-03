76876699545bcb0e2093eaadbb7c30728420f441
Allison Carte <cartemack@gmail.com>
2022-11-03 09:16:56 -0400

    Added the ability to view all user profiles

--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using TabloidMVC.Models;
+using System.Linq;
 using TabloidMVC.Utils;
 
 namespace TabloidMVC.Repositories
@@ -50,6 +52,53 @@ namespace TabloidMVC.Repositories
                     reader.Close();
 
                     return userProfile;
+                }
+            }
+        }
+
+        public List<UserProfile> GetAllUserProfiles()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                       SELECT u.id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                       u.CreateDateTime, u.ImageLocation, u.UserTypeId,
+                       ut.[Name] AS UserTypeName
+                       FROM UserProfile u
+                       LEFT JOIN UserType ut ON u.UserTypeId = ut.id";
+
+                    var reader = cmd.ExecuteReader();
+
+                    List<UserProfile> userProfiles = new();
+                    while (reader.Read())
+                    {
+                        UserProfile userProfile = new()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                            ImageLocation = DbUtils.GetNullableString(reader, "ImageLocation"),
+                            UserTypeId = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                            UserType = new UserType()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                                Name = reader.GetString(reader.GetOrdinal("UserTypeName"))
+                            },
+                        };
+
+                        userProfiles.Add(userProfile);
+                    }
+
+                    reader.Close();
+
+                    List<UserProfile> sortedProfiles = userProfiles.OrderBy(userProfile => userProfile.DisplayName).ToList();
+                    return sortedProfiles;
                 }
             }
         }
